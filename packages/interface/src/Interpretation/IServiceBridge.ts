ed7b6003995769d8d3d157f31e06ed0809b9850a
Attila Bagossy <bagossyattila@outlook.com>
2021-10-19 10:34:16 +0200

    #2/feat: introduced callServiceAppeared and callServiceDisappeared

--- packages/interface/src/Interpretation/IServiceBridge.ts
+++ packages/interface/src/Interpretation/IServiceBridge.ts
@@ -3,6 +3,8 @@ import { Metadata } from '../metadata/Metadata'
 export interface IServiceBridge {
 	respectDelayedStart(): Promise<IServiceBridge>
 	callLifecycleMethodOnService(symbol: Metadata.ServiceLifecycle): Promise<IServiceBridge>
+	callServiceAppeared(parameters: any[]): Promise<IServiceBridge>
+	callServiceDisappeared(parameters: any[]): Promise<IServiceBridge>
 	callNetworkEvent(symbol: Metadata.NetworkEvent, parameters: any[]): Promise<IServiceBridge>
 	serviceMethodHandler(cb: (externalName: string, fn: any) => void): IServiceBridge
 	serviceEventHandler(cb: (externalName: string, fn: any) => void): IServiceBridge
